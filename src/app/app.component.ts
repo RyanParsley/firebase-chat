6386914cabfeeb0aca61d1189e9cd12b1c6f4494
Ryan Parsley <ryan.parsley@rockfishinteractive.com>
2017-08-25 15:25:43 -0400

    stub out basic firebase connectivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items: FirebaseListObservable<any[]>;
+  user: Observable<firebase.User>;
+
+  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.items = db.list('/items');this.items = db.list('/items');
+    this.user = afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
   title = 'app';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDWcErHOG_QX5Z9FKUDLo4VmI6bzU9eTcQ',
+    authDomain: 'chat-59efe.firebaseapp.com',
+    databaseURL: 'https://chat-59efe.firebaseio.com',
+    projectId: 'chat-59efe',
+    storageBucket: '',
+    messagingSenderId: '372555709254'
+  }
 };
